f761aee8ff6f55b71c7a954664b43c53d10d0486
gommorin <luis.gomezmorin@gmail.com>
2020-10-05 21:31:10 -0500

    ejercicio tarea + request.method

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-const http = require('http')
+// PRÁCTICA PARA LUNES
+/*
+mandar repositorio con:
+- Responder diferente con cada ruta:
+    /hola -> la respuesta debe ser 'hola koder, bienvenido'
+    /adios -> la respuesta debe ser 'hasta la vista babe'
+ */
+
+ /* const http = require('http')
 
 const server = http.createServer((request, response) => {
     console.log('Petición entrante ->' + request.url)
@@ -14,13 +22,23 @@ const server = http.createServer((request, response) => {
 server.listen(8080, () => {
     console.log('El servidor está escuchando en el puerto 8080')
 })
+*/
 
 
-// PRÁCTICA PARA LUNES
-/*
-mandar repositorio con:
-- Responder diferente con cada ruta:
-    /hola -> la respuesta debe ser 'hola koder, bienvenido'
-    /adios -> la respuesta debe ser 'hasta la vista babe'
- */
+const http = require('http')
 
+const server = http.createServer((request, response) => {
+    console.log('Petición entrante ->' + request.url)
+    console.log('Method:', request.method)
+    if(request.url === '/hola'){
+        response.write('Hola koder, bienvenido')
+    } else if (request.url === '/adios') {
+        response.write('Hasta la vista babe')
+    }
+    response.end()
+
+})
+
+server.listen(8080, () => {
+    console.log('El servidor está escuchando en el puerto 8080')
+})
