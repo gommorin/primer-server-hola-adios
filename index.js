435315a1bf5357210330202aebaed78297935841
gommorin <luis.gomezmorin@gmail.com>
2020-10-05 19:25:38 -0500

    ejercicio-tarea-hola-adios

---
+++ index.js
@@ -0,0 +1,26 @@
+const http = require('http')
+
+const server = http.createServer((request, response) => {
+    console.log('Petición entrante ->' + request.url)
+    if(request.url === '/hola'){
+        response.write('Hola koder, bienvenido')
+    } else if (request.url === '/adios') {
+        response.write('Hasta la vista babe')
+    }
+    response.end()
+
+})
+
+server.listen(8080, () => {
+    console.log('El servidor está escuchando en el puerto 8080')
+})
+
+
+// PRÁCTICA PARA LUNES
+/*
+mandar repositorio con:
+- Responder diferente con cada ruta:
+    /hola -> la respuesta debe ser 'hola koder, bienvenido'
+    /adios -> la respuesta debe ser 'hasta la vista babe'
+ */
+
