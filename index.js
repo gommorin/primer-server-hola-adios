b14dc8857c20f0a85c4be29ecfebe566608d2a3d
gommorin <luis.gomezmorin@gmail.com>
2020-10-06 19:36:40 -0500

    tarea 3 con métodos POST y GET

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ server.listen(8080, () => {
 })
 */
 
-
+/*
 const http = require('http')
 
 const server = http.createServer((request, response) => {
@@ -34,6 +34,42 @@ const server = http.createServer((request, response) => {
         response.write('Hola koder, bienvenido')
     } else if (request.url === '/adios') {
         response.write('Hasta la vista babe')
+    }
+    response.end()
+
+})
+
+server.listen(8080, () => {
+    console.log('El servidor está escuchando en el puerto 8080')
+})
+*/
+
+/* TAREA 2:
+Hacer que las rutas reaccionen a métodos GET y POST
+POST /hola -> hola koders, estás intentando crear
+GET /hola -> hola koder, estás intentando obtener
+POST /adios -> hasta la vista baby con un post
+GET /adios -> hasta la vista baby con un get
+
+
+TAREA 3:
+    - Investigar como cambiar el header Content-Type de la respuesta
+    - Cambiar el header content type de la respuesta por el MIMEType de html
+*/
+
+const http = require('http')
+
+const server = http.createServer((request, response) => {
+    console.log('Petición entrante ->' + request.url)
+    console.log('Method:', request.method)
+    if(request.url === '/hola' && request.method === 'POST'){
+        response.write('hola koders, estás intentando crear')
+    } else if(request.url === '/hola' && request.method === 'GET'){
+        response.write('hola koder, estás intentando obtener')
+    } else if(request.url === '/adios' && request.method === 'POST'){
+        response.write('hasta la vista baby con un post')
+    } else if (request.url === '/adios' && request.method === 'GET'){
+        response.write('hasta la vista baby con un get')
     }
     response.end()
 
